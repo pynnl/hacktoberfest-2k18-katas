0b94b9a1a010aeb564be9a3ca7c433f2a063ecef
Pynnl <ryanvu.programmer@outlook.com>
2018-10-04 22:46:29 -0400

    add implementation

--- src/floodArea.js
+++ src/floodArea.js
@@ -1,3 +1,21 @@
-export const floodArea = () => {
-  // TO IMPLEMENT IN ANOTHER PR
-};
+export const floodArea = (a, x, y) => {
+  let ps = [{ x, y }]             //points to be flooded
+  a = a.map(y => y.split(''))     //convert area to matrix
+  
+  while (ps.length) {
+    let p = ps.pop()
+    let x = p.x, y = p.y
+
+    if (a[y] && a[y][x] == ' ') { //only blood if point is blank
+      a[y][x] = '~'
+
+      ps.push({ x: x - 1, y })    //setup next points
+      ps.push({ x: x + 1, y })
+      ps.push({ x, y: y - 1 })
+      ps.push({ x, y: y + 1 })
+    }
+  }
+
+  a = a.map(y => y.join(''))      //convert matrix back to area
+  return a                        //return
+}
